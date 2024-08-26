b7a3c552dd4902e5c8c546094e33edbc57882407
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-08-26 14:45:16 +0200

    tränat lite på If satser

--- Kapitel-1/Registrering/Program.cs
+++ Kapitel-1/Registrering/Program.cs
@@ -8,4 +8,17 @@ String förnamn = Console.ReadLine();
 Console.WriteLine("ange efternamn");
 String efternamn = Console.ReadLine();
 Console.WriteLine("Hej " + förnamn + " " + efternamn);
-Console.WriteLine($"Hej (förnamn) (efternamn)");
+Console.WriteLine($"Hej {förnamn} {efternamn}");
+
+// är det Valentin som loggar in?
+if (förnamn == "Valentin")
+{
+    Console.WriteLine("Vad trevligt att du är tillbaka");
+}
+
+// är det Valentin fuenzalida som loggar in?
+
+if (efternamn == "Fuenzalida")
+{
+    Console.WriteLine("Vad trevligt att du är tillbaka");
+}
