6582af727fbbb16dbd0e0016cac576927539f542
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-08-26 14:36:27 +0200

    Tränat på cw, cr writereadline och svenska teken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Hej Världen");
+Console.WriteLine("Hej Världen, nu mitt första c# program");
+Console.Write("Vad heter du?");
+string namn = Console.ReadLine();
+
+
+Console.WriteLine("Hej " + namn);
+Console.Write("Vad gör du på fritiden?");
+string fritiden = Console.ReadLine();
+Console.WriteLine("jag gör också det på fritiden");
---
+++ Kapitel-1/Registrering/Program.cs
@@ -0,0 +1,11 @@
+﻿//program för att registrera förnamn och efternamn
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("program för att registrera förnamn och efternamn");
+Console.WriteLine("ange förnamn");
+String förnamn = Console.ReadLine();
+Console.WriteLine("ange efternamn");
+String efternamn = Console.ReadLine();
+Console.WriteLine("Hej " + förnamn + " " + efternamn);
+Console.WriteLine($"Hej (förnamn) (efternamn)");
