60d8e876b8a5a2388d18e0da4e45c7ac84936943
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-09-25 12:38:38 +0200

    Testar för första gånger indexOf

--- Kapitel-2/Testa-strängar/Program.cs
+++ Kapitel-2/Testa-strängar/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//skapa en variable som är min gmail
+string email= "valentin.fuenzalidaribbing@elev.ga.ntig.se";
+ 
+
+//Hitta positionen för @-tecknet!
+int position = email.IndexOf("@");
+Console.WriteLine($"@-tecknet finns på position {position}"); 
