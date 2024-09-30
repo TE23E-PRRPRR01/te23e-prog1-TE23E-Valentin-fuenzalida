1014330f4adaff3e20d46ec4e6d0dcfc884da7b4
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-09-30 11:39:22 +0200

    Använde list<> för första gången

--- Kapitel-2/Stränghantering1-6/Program.cs
+++ Kapitel-2/Stränghantering1-6/Program.cs
@@ -3,3 +3,4 @@
 Console.WriteLine("Första stränghanteringen");
 
 String mening = "Den mätta dagen, den är aldrig störst. Den bästa dagen är en dag av törst";
+
---
+++ Kapitel-5/Samlingar/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+// Här skapas en tom lista
+List<string> länder = [];
+
+// så här fyller vi listan med värden
+länder.Add("Sverige");
+länder.Add("Norge");
+länder.Add("danmark");
+
+// skriv ut alla länder
+Console.WriteLine($"du har registrerat länderna {länder[0]}, {länder[1]} och {länder[2]}");
+
+// innehållet i listan
+List<string> länder1 = ["Sverige", "Norge", "Danmark"];
+Console.WriteLine($"du har registrerat länderna {länder[0]}, {länder[1]} och {länder[2]}");
+
+
+// innehållet i listan
+Console.WriteLine($"Tre länder i Europa: {länder[0]}, {länder[1]} och {länder[2]}");
+
+// Här skapar vi en lista på 3 heltal, vilket är ett exempel på en lista som innehåller årtal
+List<int> årtal = [1990, 2000, 2010];
+
+// här skriver vi ut innehållet i listan
+Console.WriteLine($"Tre årtal: {årtal[0]}, {årtal[1]} och {årtal[2]}");
+
+
