44251c597872bd96551c13308c2e94f274c5265a
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-18 17:31:02 +0100

    Har tränat på indexof och dela upp ett ord

---
+++ Kapitel-2/Uppgift 2.7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program ska byta håll på orden i en två ords mening");
+
+Console.WriteLine("Vänligen skriv en mening men två ord och utan punkt");
+string svar = Console.ReadLine();
+
+int Mellanslag = svar.IndexOf(" ");
+
+string förstaordet = svar[..Mellanslag];
+string andraordet = svar[(Mellanslag + 1)..];
+
+Console.WriteLine(andraordet + " " + förstaordet);
