cd3e814e092c95da0579797d5968a0a08eaf0b28
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-18 17:40:52 +0100

    Tränat med omvandling från string till int

---
+++ Kapitel-2/Uppgift 2.8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Skriv in en addition");
+string addition = Console.ReadLine();
+int plusindex = addition.IndexOf("+");
+string tal1Text = addition[..plusindex];
+string tal2Text = addition[(plusindex + 1)..];
+
+int tal1 = int.Parse(tal1Text);
+int tal2 = int.Parse(tal2Text);
+int summa = tal1 + tal2;
+
+Console.WriteLine(addition + " = " + summa);
