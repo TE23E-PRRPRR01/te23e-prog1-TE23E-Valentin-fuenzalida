b4a84c340ec88b61c4613714165d73ad22f8ee02
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-18 17:44:18 +0100

    Har tränat på $""

---
+++ Kapitel-2/Uppgift 2.9/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program ska hälsa användaren och säga hens namn");
+
+Console.WriteLine("Vad heter du? ");
+string svar = Console.ReadLine();
+
+Console.WriteLine($"Hej {svar} vad trevligts att träffas");
