85b83ce6e5f2118d27bdbdf2906c597d5a954bd3
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-18 14:05:08 +0100

    Tränat med tryparse, for-loop och while-loop

---
+++ Kapitel-4/Mikroövning/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.WriteLine("mikroövning");
+
+int resultat = 0;
+
+while (true)
+{
+
+        Console.WriteLine("Hur många repititioner vill du ha");
+        bool lyckad = int.TryParse(Console.ReadLine(), out resultat);
+
+    if (resultat > 0 && resultat < 11)
+    {
+        List<string> listanamn = ["Valentin"];
+
+        for (int i = 0; i < resultat; i++)
+        {
+            Console.WriteLine($"{i + 1}. {listanamn[0]}");
+        }
+
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("repitioner måste vara mellan intervallet 1 till 10!!");
+    }
+
+}
+
+
+Console.WriteLine("skriv in ett tal");
