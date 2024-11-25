7abbadc1b1aee11bf93eef6f047051e712b6b8fb
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-25 17:35:21 +0100

    tränat på booleska uttryck i if satser

--- Kapitel-3/Uppgift 3.6/Program.cs
+++ Kapitel-3/Uppgift 3.6/Program.cs
@@ -1,4 +1,20 @@
 ﻿Console.Clear();
 
 
-Console.WriteLine("");
+Console.WriteLine("Detta program ska ta reda på om du får åka i en berg-och-dalbana");
+
+Console.WriteLine("hur lång är du (svara i meter): ");
+double längd = double.Parse(Console.ReadLine());
+
+if (längd >= 1.5 && längd <= 1.9)
+{
+    Console.WriteLine("du får åka i berg-och-dalbanan");   
+}
+else if (längd < 1.5)
+{
+    Console.WriteLine("Du är för kort för denna berg-och-dalbanan");
+}
+else if (längd > 1.9)
+{
+    Console.WriteLine("Du är för lång för berg-och-dalbanan");
+}
---
+++ Kapitel-3/Uppgift 3.7/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program ska få reda på om du får köpa kaffe för ett billigare pris");
+
+Console.WriteLine("Hur gammal är du? ");
+int ålder = int.Parse(Console.ReadLine());
+
+if ((ålder >= 10 && ålder <= 18) || ålder >= 65)
+{
+    Console.WriteLine("Du får köpa kaffe billigare");
+}
+else
+{
+    Console.WriteLine("Du får inte köpa kaffe billigt");
+}
