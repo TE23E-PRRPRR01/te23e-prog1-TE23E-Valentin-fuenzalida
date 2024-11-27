2c4f8a507fcf2cfa6325bb1c351192bdc76aff4b
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-27 14:08:59 +0100

    Jag har tränat på ComparTo och Contains

---
+++ Kapitel-3/Uppgift 3.8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("Användaren ska skriva in 3 ord och programmet ska lista ut vilket ord som är i först i bokstavsordning");
+
+Console.WriteLine("Skriv in ett ord");
+string ord1 = Console.ReadLine();
+
+Console.WriteLine("skriv in ett till ord");
+string ord2 = Console.ReadLine();
+
+Console.WriteLine("skriv in ett till ord");
+string ord3 = Console.ReadLine();
+
+
+if (ord1.CompareTo(ord2) < 0 && ord1.CompareTo(ord3) < 0)
+{
+    Console.WriteLine($"{ord1} kommer före både {ord2} och {ord3} i bokstavsordning");
+}
+else
+{
+    Console.WriteLine($"{ord1} kommer ej före både {ord2} och {ord3} i bokstavsordning");
+}
---
+++ Kapitel-3/Uppgift 3.9/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("detta program är där användaren får skriva in en valfri sträng. Programmet ska sedan berätta ifall strängen innehåller...");
+
+Console.WriteLine("Skriv in en valfri text");
+string sträng = Console.ReadLine();
+
+
+if (sträng.Contains("+") && sträng.Contains("-"))
+{
+Console.WriteLine("strängen innehåller både ett plustecken och ett minustecken");
+}
+else if (sträng.Contains("-"))
+{
+    Console.WriteLine("strängen innehåller ett minustecken");
+}
+else if (sträng.Contains("+"))
+{
+    Console.WriteLine("strängen innehållet ett plustecken");
+}
+else
+{
+    Console.WriteLine("strängen innehåller varken ett plustecken eller ett minustecken");   
+}
