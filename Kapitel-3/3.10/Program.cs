d385ed62e54a96489bc905c4e93e54a8ca9fff87
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-27 19:58:25 +0100

    Tränade på Switch

---
+++ Kapitel-3/3.10/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta progam ska berätta om du fick guld, silver, bronze eller ingen medalj i förhållande till användarens senaste idrottsturning");
+
+Console.WriteLine("Vilken plays kom du på? ");
+string plats = Console.ReadLine();
+
+switch (plats)
+{
+    case "1":
+        Console.WriteLine("Du fick guld!");
+        break;
+    case "2":
+        Console.WriteLine("Du fick silver!");
+        break;
+    case "3":
+        Console.WriteLine("Du fick bronze!");
+        break;
+    default:
+        Console.WriteLine("Du fick ingen medalj");
+        break;
+}
---
+++ Kapitel-3/3.11/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program är en frågesport däremot får användaren välja en katagori");
+
+Console.WriteLine(@"
+Välj en frågekategori.
+1. Musik
+2. Geografi
+3. Historia
+");
+string katagori = Console.ReadLine();
+string rättsvar = "";
+switch (katagori)
+{
+    case "1":
+    Console.WriteLine("Vad heter artisten till sången Arcade");
+    rättsvar = "Duncan Laurence";
+    break;
+    case "2":
+    Console.WriteLine("Vilken huvudstad har Tyskland");
+    rättsvar = "Berlin";
+    break;
+    default:
+    Console.WriteLine("Mellan vilka år hände andra världskriget");
+    rättsvar = "1939-1945";
+    break;
+}
+
+// läser in användarens svar
+string svar = Console.ReadLine().ToLower();
+
+if (svar == rättsvar)
+{
+    Console.WriteLine("Du hade rätt!");
+}
+else
+{
+    Console.WriteLine("Du hade fel!");
+}
