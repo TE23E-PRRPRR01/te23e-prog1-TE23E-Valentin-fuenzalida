f4fce1c58ce563b9606ee9359ed8f2cd7370b8fc
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-30 13:17:05 +0100

    Tränade på if satser med en varibel inne i if satser

--- Kapitel-3/Uppgift 3.15/Program.cs
+++ Kapitel-3/Uppgift 3.15/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Detta program ska räkna ut hyre räkningen");
+
+Console.WriteLine("Hur många timmar vill du hyra bilen? (svara i mindre än en dag)");
+int timmar = int.Parse(Console.ReadLine());
+
+int summa = timmar * 80;
+
+if (summa > 950)
+{
+    summa = 950;
+}
+
+Console.WriteLine($"Din hyra blir {summa} för {timmar} timmar");
