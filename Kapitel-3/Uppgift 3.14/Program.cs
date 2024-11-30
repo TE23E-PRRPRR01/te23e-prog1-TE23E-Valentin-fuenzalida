bbcbdba11658a9766a5015f2f19021ce19092466
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-30 12:35:01 +0100

    Arbetade med if satser för både string och int

---
+++ Kapitel-3/Uppgift 3.14/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program ska bestämma om ett företag vill anställa användaren");
+
+Console.WriteLine("Har du går ut gymnasiet? (Svara med j/n)");
+string Svar = Console.ReadLine();
+
+
+Console.WriteLine("Hur gammal är du? ");
+int ålder = int.Parse(Console.ReadLine());
+
+if (Svar == "j" && ålder < 22)
+{
+    Console.WriteLine("Vi vill gärna anställa dig");
+}
+else
+{
+    Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+}
