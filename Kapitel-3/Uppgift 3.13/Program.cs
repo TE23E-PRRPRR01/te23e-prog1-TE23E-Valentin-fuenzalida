dc5c2bc78127d54ba546c2324d515d52f452b3f1
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-11-30 12:23:13 +0100

    Tränade på if satser

---
+++ Kapitel-3/Uppgift 3.13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program ska få reda på om användaren är gammal nog att delta i en tävling");
+
+Console.WriteLine("Hur gammal är du? ");
+int ålder = int.Parse(Console.ReadLine());
+
+
+if (ålder >= 16 && ålder <= 19)
+{
+    Console.WriteLine("Du får vara med i tävlingen");
+}
+else if (ålder > 19)
+{
+    Console.WriteLine("Du är för gammal för att vara med i tävlingen");
+}
+else
+{
+    Console.WriteLine("Du är för ung för att vara med i tävlingen");
+}
