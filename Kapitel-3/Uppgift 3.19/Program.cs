ed69c4abe2c6ffa23f467c8c11701e1655bd9a17
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-02 13:08:57 +0100

    jobbat med if och indexOf

---
+++ Kapitel-3/Uppgift 3.19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program är för att räkna ut en subration eller addition");
+
+Console.WriteLine("skriv in en addition eller subration mellan två tal");
+string räkning = Console.ReadLine();
+
+if (räkning.Contains("+"))
+{
+    int plusteckenindex = räkning.IndexOf("+");   
+}
+else if (räkning.Contains("-"))
+{
+    
+}
+else
+{
+    
+}
+
