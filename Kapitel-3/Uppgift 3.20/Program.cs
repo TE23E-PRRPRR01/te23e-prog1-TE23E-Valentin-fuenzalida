6c7412cae47af0de7e276ab811cd416557cc7fe0
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-03 09:25:34 +0100

    Jag har tränat på compareto med if satser

---
+++ Kapitel-3/Uppgift 3.20/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program ska lista ut vilka av 3 ord är först i bokstavsordning");
+
+Console.WriteLine("skriv in ett ord: ");
+string ord1 = Console.ReadLine();
+Console.WriteLine("skriv in ett ord: ");
+string ord2 = Console.ReadLine();
+Console.WriteLine("skriv in ett ord: ");
+string ord3 = Console.ReadLine();
+
+if (ord1.CompareTo(ord2) < 0 && ord1.CompareTo(ord3) < 0)
+{
+    Console.WriteLine($"{ord1} är först i bokstavsordning av orden {ord1}, {ord2} och {ord3}");
+}
+else if (ord2.CompareTo(ord1) < 0 && ord2.CompareTo(ord3) < 0)
+{
+    Console.WriteLine($"{ord2} är först i bokstavsordning av orden {ord1}, {ord2} och {ord3}");
+}
+else
+{
+    Console.WriteLine($"{ord3} är först i bokstavsordning av orden {ord1}, {ord2} och {ord3}");
+}
