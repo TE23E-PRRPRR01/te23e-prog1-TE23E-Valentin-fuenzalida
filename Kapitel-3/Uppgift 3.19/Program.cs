54d6c464cd649777719ef47fe99e734d8283e1a1
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-03 08:41:37 +0100

    Har jobbat med if satser och indexOf

--- Kapitel-3/Uppgift 3.19/Program.cs
+++ Kapitel-3/Uppgift 3.19/Program.cs
@@ -7,14 +7,25 @@ string räkning = Console.ReadLine();
 
 if (räkning.Contains("+"))
 {
-    int plusteckenindex = räkning.IndexOf("+");   
+    int plusteckenindex = räkning.IndexOf("+");
+    double förstatal = double.Parse(räkning [..plusteckenindex]);
+    double andratal = double.Parse(räkning[(plusteckenindex + 1)..]);
+
+    double addition =  förstatal + andratal; 
+
+    Console.WriteLine($"Additionen av de två talen är {addition}");
 }
 else if (räkning.Contains("-"))
 {
-    
+    int minusteckenindex = räkning.IndexOf("-");
+    double förstatal = double.Parse(räkning[..minusteckenindex]);
+    double andratal = double.Parse(räkning[(minusteckenindex + 1)..]);
+
+    double subration = förstatal - andratal;
+    Console.WriteLine($"Subtration av de två talen är {subration}");
 }
 else
 {
-    
+    Console.WriteLine("du måste välja antingen än addition eller en subration");
 }
 
