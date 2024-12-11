4d2d0fc318bf0adb111f6ec17999269f5cba8385
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-11 14:21:51 +0100

    Tränat på menyprogram med switch istället för if

---
+++ Kapitel-4/Uppgift 4.3/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+Console.WriteLine("Menyprogram");
+
+string användarensval = "";
+
+while (användarensval != "3")
+{
+    Console.WriteLine();
+    Console.Write(@"Välj ett av alternativen
+    1. Omvandla meter till kilometer
+    2. Omvandla kilometer till meter
+    3. Avsluta programmet
+    ");
+    användarensval = Console.ReadLine();
+
+    switch (användarensval)
+    {
+        case "1":
+        Console.WriteLine("skriv in en längd: (i meter)");
+        double tal = double.Parse(Console.ReadLine());
+        Console.WriteLine($"{tal} i kilometer är {tal/1000}");
+        break; 
+        Console.WriteLine();
+
+        case "2":
+        Console.WriteLine("skriv in en längd: (i kilometer)");
+        double längd = double.Parse(Console.ReadLine());
+        Console.WriteLine($"{längd} i meter är {längd * 1000}");
+        break;
+        Console.WriteLine();
+
+        case "3":
+        Console.WriteLine("Programmet kommer att avslutas...");
+        break;
+        Console.WriteLine();
+
+        default:
+        Console.WriteLine("Du valde inte ett gilltigt val, försök igen");
+        break;
+    }
+}
