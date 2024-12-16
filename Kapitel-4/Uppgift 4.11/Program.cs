dc3d4b7b1611a7372c4d2ee6ff0ca0d9612a8cfe
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-16 13:53:23 +0100

    Har tränat med break, while och if

---
+++ Kapitel-4/Uppgift 4.11/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("vilket är Europas folkrikaste land");
+string svar = Console.ReadLine();
+int antalgissningar = 1;
+
+while (svar != "Tyskland".ToLower())
+{
+    if (antalgissningar >= 5)
+    {
+        Console.WriteLine("Dina gissningar är slut");
+        break;
+    }
+    Console.WriteLine("Fel svar, försök igen");
+    svar = Console.ReadLine();
+    antalgissningar++;
+}
+
+if (svar == "Tyskland".ToLower())
+{
+    Console.WriteLine("Ditt svar är rätt.");
+}
