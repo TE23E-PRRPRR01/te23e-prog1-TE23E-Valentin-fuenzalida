8955f41b3f0efc4663a8129c715bc2eb2f6ac4fd
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-16 12:40:03 +0100

    Har tränat med for-loop

---
+++ Kapitel-4/Uppgift 4.7/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Detta program ska beräkna siffersumman av ett tal användaren skriver in");
+
+Console.WriteLine();
+
+Console.WriteLine("skriv in ett tal");
+string text = Console.ReadLine();
+
+int siffersumma = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    string teckenstring = text[i].ToString();
+    int tal = int.Parse(teckenstring);
+    siffersumma += tal;
+}
+
+Console.WriteLine();
+
+Console.WriteLine($"Siffersumman av {text} är lika med {siffersumma}");
