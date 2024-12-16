2ce75c17480a86dab3da5e77138fc3f917caf527
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-16 13:17:48 +0100

    Har tränat med for-loop

---
+++ Kapitel-4/Uppgift 4.8/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+Console.WriteLine("skriv in ett tal");
+string text = Console.ReadLine();
+
+// skapar en variabler som har startvärdet som falskt
+bool Innehåller7 = false;
+
+// går igeonom varje tal om den innehåller antingen en 7 eller en 3
+for (int i = 0; i < text.Length; i++)
+{
+    // omvandlar hela strängen till enskilda strängar
+    string teckenstring = text [i].ToString();
+    // omvandlar varje enkild sträng till ett tal
+    int tal = int.Parse(teckenstring);
+    // repeterar igenom varje tal för att se om den ineehåller antingen en 7 eller en 3
+    if (text[i] == '3' || text[i] == '7')
+    {
+        Innehåller7 = true;
+    }
+}
+
+if (Innehåller7)
+{
+    Console.WriteLine("ditt tal innehöll åtminstone minst en 7:a eller en 3:a");
+}
+else
+{
+    Console.WriteLine("ditt tal innehöll varken en 7 eller en 3");
+}
---
+++ Kapitel-4/Uppgift 4.9/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.WriteLine("Säg hur många kvadrattal du vill programmet ska skriva ut (kvadrattalen kommer börja från 1)");
+int antalKvadrater = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= antalKvadrater; i++)
+{
+    Console.WriteLine($"{i}*{i} = {i*i} ");
+}
