9c438a1e05eddb49b1a2485e8914baef4f77ac48
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-17 10:43:15 +0100

    tränade på nästlade loopar

---
+++ Kapitel-4/Uppgift 4.14/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.Clear();
+
+Console.Write("Skriv in ett ord: ");
+string ord = Console.ReadLine();
+
+bool AllaTeckenMinstTvåGånger = true;
+for (int i = 0; i < ord.Length; i++)
+{
+    bool ettteckenminsttvågånger = false;
+    for (int j = 0; j < ord.Length; j++)
+    {
+        if (i != j && ord[i] == ord[j])
+        {
+            ettteckenminsttvågånger = true;
+        }
+    }
+    if (ettteckenminsttvågånger == false)
+    {
+        AllaTeckenMinstTvåGånger = false;        
+    }
+}
+if (AllaTeckenMinstTvåGånger)
+{
+    Console.WriteLine("Alla tecken förekommer minst två gånger ");
+}
+else
+{
+    Console.WriteLine("Det finns minst ett tecken som bara förekommer en gång i ordet");
+}
