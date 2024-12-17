aaa619e254185fcc0f6b9d81adc3fa0d9f0b6a82
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-17 11:08:23 +0100

    Har tränat på Thread sleep

---
+++ Kapitel-4/Uppgift 4.16/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+string meddelande = "Hej! Är 3,14 ett heltal? Tack, för att du, läste meddelandet.";
+
+for (int i = 0; i < meddelande.Length; i++)
+{
+    Console.Write(meddelande[i]);
+
+    // Tänk på att det är jämförelse mellan char, inte string
+    if (meddelande[i] == '.' || meddelande[i] == '?' || (meddelande[i] == ',' && meddelande[i + 1] == ' ') || meddelande[i] == '!')
+    {
+        Thread.Sleep(1000);
+    }
+    else
+    {
+        Thread.Sleep(100);
+    }
+}
