c9c2d4fbaa2602f3a46cfc45792a99f19d8fd4b2
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-17 10:20:20 +0100

    tränat på nästlade loopar

---
+++ Kapitel-4/Uppgift 4.13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Skriv in rektangelns bredd: ");
+int bredd = int.Parse(Console.ReadLine());
+Console.WriteLine("Skriv in rektangelns höjd");
+int höjd = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 0; i < höjd; i++)
+{
+    for (int j = 0; j < bredd; j++)
+    {
+        if (i == 0 || i == höjd - 1 || j == 0 || j == bredd - 1)
+        {
+            Console.Write("X");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
