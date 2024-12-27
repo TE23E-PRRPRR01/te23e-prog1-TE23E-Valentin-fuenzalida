21663377139bfd54c89044f78e4f675aa0547c3e
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-27 11:19:51 +0100

    tränat med while-loop

---
+++ Kapitel-4/Uppgift 4.23/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.WriteLine("skriv in ett heltal: ");
+
+int tal = int.Parse(Console.ReadLine());
+
+int störst = tal;
+
+string svar = "";
+while (svar != "n")
+{
+    Console.WriteLine("Vill du skriva in ett till heltal? (j/n)");
+    svar = Console.ReadLine();
+
+    if (svar == "n")
+    {
+        break;
+    }
+
+    Console.WriteLine("Skriv in ett till heltal: ");
+    int nytttal = int.Parse(Console.ReadLine());
+
+    if (nytttal > störst)
+    {
+        störst = nytttal;
+    }
+}
+
+Console.WriteLine($"Det största talet du skrev in var {störst}");
