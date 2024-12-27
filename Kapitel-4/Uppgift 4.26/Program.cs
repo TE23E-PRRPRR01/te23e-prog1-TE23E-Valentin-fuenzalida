b685602e32ecd611f302fb5dd8b45995fa70edd9
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-27 11:45:03 +0100

    for-loop

---
+++ Kapitel-4/Uppgift 4.26/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Skriv in ett tal: ");
+string taltext = Console.ReadLine();
+
+for (int i = 0; i < taltext.Length; i++)
+{
+    string tecken = taltext[i].ToString();
+    int siffra = int.Parse(tecken);
+    if (siffra == 9)
+    {
+        Console.Write("0");
+    }
+    else
+    {
+        Console.Write(siffra + 1);
+    }
+}
