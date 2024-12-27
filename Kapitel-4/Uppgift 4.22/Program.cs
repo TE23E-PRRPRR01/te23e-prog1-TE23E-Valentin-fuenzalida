52311752ff282a06434945081928886d12ad28cc
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-27 11:12:53 +0100

    tränat med räkna uppåt for-loop, räkna neråt for-loop och räkna uppåt med 50 for-loop

---
+++ Kapitel-4/Uppgift 4.22/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+for (int i = 10; i < 31; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+for (int i = 201 - 1; i >= 180 ; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+for (int i = 1000; i < 1410; i += 50)
+{
+    Console.WriteLine(i);
+}
