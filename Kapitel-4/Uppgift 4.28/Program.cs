acb2a0f7db42eda8aa4207b19ef999980a6f66f6
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2024-12-29 12:17:14 +0100

    tränat på bool, if.loop och for-loop

---
+++ Kapitel-4/Uppgift 4.28/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+Console.WriteLine("Skriv in ett ord");
+string Text = Console.ReadLine();
+Console.WriteLine("Skriv in exact två tecken");
+string Tecken = Console.ReadLine();
+
+bool Förstatecknet = false;
+bool Andratecknet = false;
+
+for (int i = 0; i < Text.Length; i++)
+{
+    if (Text[i] == Tecken[0])
+    {
+        Förstatecknet = true;
+    }
+    if (Text[i] == Tecken[1])
+    {
+        Andratecknet = true;        
+    }
+}
+
+if (Förstatecknet && Andratecknet)
+{
+    Console.WriteLine("den första texten innehåller både första och andra teckent");
+}
+else if (Förstatecknet || Andratecknet)
+{
+    Console.WriteLine("minst ett av tecknen finns i texten");
+}
+else
+{
+    Console.WriteLine("inga av de två tecknen finns i första texten");
+}
