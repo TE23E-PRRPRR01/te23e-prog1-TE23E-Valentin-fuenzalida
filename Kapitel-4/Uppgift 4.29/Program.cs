3024b0e00f049636166fa6070cab372f4ed22f6f
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2025-01-05 14:50:40 +0100

    tränat for-loop

---
+++ Kapitel-4/Uppgift 4.29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Ange triangels sidlängd: ");
+int sidlängd = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 0; i < sidlängd; i++)
+{
+    for (int j = 0; j < i + 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
