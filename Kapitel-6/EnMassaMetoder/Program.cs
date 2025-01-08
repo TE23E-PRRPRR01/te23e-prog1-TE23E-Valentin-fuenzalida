0380f97e212927318ee4bb217b3c7c0b844aeeea
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2025-01-08 15:03:20 +0100

    tränat med metoder

---
+++ Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,33 @@
+﻿/* *************************************************************
+* **********************     M A I N            ***********************
+**************************************************************** */
+
+
+Console.Clear();
+
+SägHejNamn("Valentin");
+
+KvadratTal(3);
+
+/* *************************************************************
+* ********************* M E T O D E R *************************
+**************************************************************** */
+
+/// <summary>
+/// Hälsar med input
+/// </summary>
+/// <param name="namn">Namnet</param>
+static void SägHejNamn(string namn)
+{
+    Console.WriteLine($"Hej på {namn}!");
+}
+
+/// <summary>
+/// Metod som kvaderar ett tal
+/// </summary>
+/// <param name="Tal"></param>
+
+static void KvadratTal(int Tal)
+{
+    Console.WriteLine($"{Tal} i kvadrat = {Tal*Tal}");
+}
