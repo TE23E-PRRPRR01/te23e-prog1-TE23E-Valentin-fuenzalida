8306d88a32dec88c625f201bdc56c6280424b826
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2025-01-20 14:27:20 +0100

    föränklar

---
+++ Kapitel-6/MetoderFörText/Program.cs
@@ -0,0 +1,74 @@
+﻿Console.Clear();
+
+Console.WriteLine("Uppfigt 16");
+
+static int Taremot()
+{
+    string Antalbokstäver = Console.ReadLine();
+    int räknabokstäver = Antalbokstäver.Length;
+    return räknabokstäver;
+}
+
+Console.WriteLine("ange ett tal");
+
+Console.WriteLine(Taremot());
+
+
+Console.WriteLine();
+
+
+Console.WriteLine("Uppgift 17");
+
+static int Vokaler()
+{
+    int antal = 0;
+    string ord = Console.ReadLine().ToLower();
+    foreach (var Vokaler in ord)
+    {
+        if (ord.Contains(/[aeiou]/))
+        {
+            antal++;
+        }
+        else if (ord.Contains("e"))
+        {
+            antal++;
+        }
+        else if (ord.Contains("i"))
+        {
+            antal++;
+        }
+        else if (ord.Contains("o"))
+        {
+            antal++;
+        }
+        else if (ord.Contains("u"))
+        {
+            antal++;
+        }
+        else if (ord.Contains("y"))
+        {
+            antal++;
+        }
+        else if (ord.Contains("å"))
+        {
+            antal++;
+        }
+        else if (ord.Contains("ä"))
+        {
+            antal++;
+        }
+        else if (ord.Contains("ö"))
+        {
+            antal++;
+        }
+        else
+        {
+            antal = 0;
+        }
+    }
+    return antal;
+}
+
+Console.WriteLine("skriv ett ord");
+
+Console.WriteLine(Vokaler());
---
+++ Kapitel-6/Refaktorisering/Program.cs
@@ -0,0 +1,107 @@
+﻿Console.Clear();
+
+/* *************************************************
+ * Namn: Konsolgrafik
+ * Beskrivning: Ett program för att rita konsolgrafik
+ * Datum: 2024-09-01
+ *********************************************** */
+
+Intro();
+
+while (true)
+{
+    Meny();
+    string val = Console.ReadLine();
+
+    // Hantera användarens val
+    if (val == "1")
+    {
+        int tal = LäsInHeltal();
+
+RitaFyrkant(tal);
+    }
+    else if (val == "2")
+    {
+        int tal = LäsInHeltal();
+
+        RitaTriangel(tal);
+    }
+    else if (val == "3")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ogiltigt alternativ, vg försök igen.");
+    }
+}
+
+/* **********************************************
+ *  Metoder 
+ ********************************************** */
+
+
+static void Intro()
+{
+    Console.Clear();
+    Console.WriteLine("""
+Program för att rita konsolgrafik
+===================================
+""");
+}
+Console.Clear();
+Console.WriteLine("""
+Program för att rita konsolgrafik
+===================================
+""");
+
+static void Meny()
+{
+    // Skriva ut menyn
+    Console.WriteLine("""
+    1. Skriv ut fyrkant
+    2. Skriv ut triangel
+    3. Avsluta
+    """);
+    Console.Write("Välj ett alternativ: ");
+}
+
+
+static int LäsInHeltal()
+{
+    Console.Write("Ange ett heltal: ");
+    string talString = Console.ReadLine();
+    int tal;
+    while (!int.TryParse(talString, out tal))
+    {
+        Console.Write("Ange ett giltigt heltal: ");
+        talString = Console.ReadLine();
+    }
+    return tal;
+}
+
+static void RitaFyrkant(int tal)
+{
+    // Skriv ut fyrkanten
+    for (int i = 0; i < tal; i++)
+    {
+        for (int j = 0; j < tal; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
+
+static void RitaTriangel(int tal)
+{
+            // Skriv ut triangeln
+        for (int i = 0; i < tal; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+}
